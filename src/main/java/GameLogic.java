265ff098eff6cfe57d7b984325f014f0a6bed5a4
IT <ransdorf@sosceskybrod.cz>
2024-03-21 08:59:14 +0100

    Update wall position commit

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -20,7 +20,7 @@ public class GameLogic {
         ball = new Ball(20, 20, 50, 50, Color.BLUE);
         ballEnemy = new Ball(250, 20, 70, 70, Color.RED);
         Wall wall1 = new Wall(300, 430, 400,430, Color.BLACK);
-        Wall wall2 = new Wall(100, 390, 150, 390, Color.BLACK);
+        Wall wall2 = new Wall(400, 20, 400, 70, Color.BLACK);
         Wall wall3 = new Wall(350,300,600,300, Color.GREEN);
         walls.add(wall1);
         walls.add(wall2);
